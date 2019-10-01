8137ce1c556efb28b309e6adb38bde306785b3e0
Colleen McGuire <mzmacky2@gmail.com>
2019-10-01 14:44:38 -0400

    good work... don't need the index values for vowels just say if word[0] in vowels:

--- pig latin.py
+++ pig latin.py
@@ -4,7 +4,7 @@ def pigLatin():
     changedConsonant = x[1:]+x[0]+"ay"
     changedVowel = x[0:]+"way"
     vowels = ["a","e","i","o","u"]
-    if x[0] in vowels[0:4]:
+    if x[0] in vowels[0:4]: #no need to select the index values ... just say in vowels:
         print(changedVowel)
     else:
         print(changedConsonant)
